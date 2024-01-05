6f15a37d5f2f105485fceb46fce2fdd518d786e0
Shaanukstar123 <shaanukag0@gmail.com>
2024-01-05 07:32:19 +0000

    Working Newsround webcrawler

--- initalTests/newsScraper/newsScraper/spiders/crawlerAgent.py
+++ initalTests/newsScraper/newsScraper/spiders/crawlerAgent.py
@@ -1,68 +1,43 @@
-import scrapy
+import re
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-from scrapy.crawler import CrawlerProcess
-from scrapy.http import Request
 
-class CrawlingAgent(CrawlSpider):
-    name = "timeForKids"
-    allowed_domains = ["timeforkids.com"] 
-    start_urls = [
-        "https://www.timeforkids.com/g56/"
-    ]
-    rules = (
-        Rule(LinkExtractor(allow=('[^/]+$'))),
-    )
-
-    # def parse(self, response):
-    #     self.logger.info('Hi, this is an item page! %s', response.url)
-    #     item = scrapy.Item()
-    #     item['url'] = response.url
-    #     return item
-class ArticleItem(scrapy.Item):
-    title = scrapy.Field()
-    text = scrapy.Field()
 class CrawlingAgent(CrawlSpider):
     name = "newsRound"
-    allowed_domains = ["bbc.co.uk"] 
+    allowed_domains = ["bbc.co.uk"]
     start_urls = ["https://www.bbc.co.uk/newsround"]
-    
+
+    # Setting up a rule to allow scraping in the specific article structure
     rules = (
-        Rule(LinkExtractor(allow=('/newsround/[6-9][6-9]\d*$')), callback='collect_links'),
+        Rule(LinkExtractor(allow=('/newsround/[6-9]{2}\d*$')), callback='parse_item', follow=True),
     )
 
-    def __init__(self, *args, **kwargs):
-        super(CrawlingAgent, self).__init__(*args, **kwargs)
-        self.article_links = []
+    def parse_item(self, response):
+        # Extract article content using XPath or CSS selectors
+        # Assuming article titles are within <h1> tags and contents within <p> tags
+        article_name = response.xpath('//h1/text()').get() or response.css('h1::text').get()
+        article_content = ' '.join(response.xpath('//p/text()').getall())
 
-    def parse_article(self, response):
-        print("Entered parse_article")  # Debug: Confirm entering the method
-        item = ArticleItem()
-        item['title'] = response.xpath('//h1/text()').get()  # Update this XPath according to the site's structure
-        item['text'] = response.xpath('//div[@class="article-text"]/p//text()').get()  # Update this XPath
-        print("Parsed article:", item['title'])  # Debug: Print the title
-        yield item
+        # Only proceed if both name and content could be found
+        if article_name and article_content:
+            article_id = int(re.findall(r'\d+$', response.url)[0])  # Extracts ID from URL
+            return {
+                'id': article_id,
+                'name': article_name.strip(),
+                'content': article_content.strip()
+            }
 
-    def collect_links(self, response):
-        # Collecting links
-        numeric_id = int(response.url.split('/')[-1])
-        self.article_links.append((numeric_id, response.url))
-        
-        # Once enough links are collected, sort and yield requests for top 30 articles
-        if len(self.article_links) >= 30:  
-            self.article_links.sort(reverse=True, key=lambda x: x[0])
-            top_30_links = self.article_links[:30]
-
-            for _, url in top_30_links:
-                print("Yielding request for:", url)
-                yield Request(url, callback=self.parse_article)
-
-# Main runner
+# The main execution
 if __name__ == "__main__":
+    # Set up the crawler and start it
+    from scrapy.crawler import CrawlerProcess
+
     process = CrawlerProcess(settings={
-        "FEEDS": {
-            "articles.json": {"format": "json"},
-        },
+        'FEED_FORMAT': 'json',           # Set format of the output file
+        'FEED_URI': 'output.json',       # Name of the output file
+        'LOG_LEVEL': 'INFO',             # Set logging level
+        'CLOSESPIDER_ITEMCOUNT': 10      # Close spider after scraping 10 items
     })
+
     process.crawl(CrawlingAgent)
-    process.start()
+    process.start()  # the script will block here until the crawling is finished
