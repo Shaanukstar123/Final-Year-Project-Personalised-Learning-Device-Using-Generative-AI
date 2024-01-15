80a6d313f9afea451e181f1bf7db8693075a75ab
Shaanukstar123 <shaanukag0@gmail.com>
2024-01-15 00:11:02 +0000

    Defined images with DALL-E-3

--- MVP/Backend/imageGenerator.py
+++ MVP/Backend/imageGenerator.py
@@ -1,15 +1,14 @@
 from flask import Flask, jsonify
 from openai import OpenAI
-import json
 
-app = Flask(__name__)
 client = OpenAI(api_key='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')
 
 def generateImageWithDALLE(prompt):
     response = client.images.generate(
         prompt=prompt,
         n=1,  # Number of images to generate
-        size="512x512"  # Size of the image
+        size="1024x1024",  # Size of the image
+        model = "dall-e-3"
     )
     # Assuming the API returns a list of generated images
     image_url = response.data[0].url  # or however the URL is returned in the response
