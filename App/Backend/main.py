fea2875d08b9ec303cfd20b7e692e58dd85cc599
Shaanukstar123 <shaanukag0@gmail.com>
2024-03-05 05:15:06 +0000

    Updated backend for continuous stories

--- App/Backend/main.py
+++ App/Backend/main.py
@@ -3,15 +3,18 @@ from flask_cors import CORS
 # from threading import Thread
 from webCrawler.newsGrabber import run_crawler
 from articleNamerGPT import generateNewNames
-from storyChain import storyChain
+from storyChain import initialiseStory, initialiseModel, continueStory
 from imageGenerator import generateImageWithDALLE
 from textSummariser import summariseText
 import concurrent.futures
+from flask import request
 
 import json
 
 app = Flask(__name__)
 CORS(app) 
+
+'''ROUTES'''
 
 @app.route('/update_news', methods=['GET'])
 def start_crawler():
@@ -26,6 +29,28 @@ def get_topics():
         return jsonify(data)
     
 
+@app.route('/fetch_story/<id>', methods=['GET'])
+def get_story(id):
+        articleContent = getArticleContent(id)
+        if articleContent:
+            story, image_url = generateFirstPage(articleContent,storyChain)
+            return jsonify({"story": story, "image_url": image_url})
+        else:
+            return jsonify({"error": "Article not found"}), 404
+        
+        
+@app.route('/continue_story/<id>', methods=['POST'])
+def continue_story(id):
+    # Extract user input from the request
+    user_input = request.json.get('user_input', '')
+    # Continue the story
+    output, imageUrl = generateNextPage(user_input, storyChain)
+    return jsonify({"story": output, "image_url": imageUrl})
+
+
+
+'''Helper Functions'''
+
 def getArticleContent(id):
     with open('data/output.json', 'r') as file:
         data = json.load(file)
@@ -33,37 +58,42 @@ def getArticleContent(id):
             if int(article['id']) == int(id):
                 return article['content']
     return None
+
     
-def generateStoryAndImage(article):
+def generateImage(text):
+    if "DALL-E Prompt:" in text:
+        summary = text.split("DALL-E Prompt:")[1]
+        print("DALLE prompt found:")
+        return summary
+        
+    print("No DALLE prompt")
+    dallePrompt = summariseText(text)
+    return dallePrompt
+
+def generateFirstPage(article, chain):
     # Generate story
     print("Generating story...")
-    story = storyChain(article)
+    story = initialiseStory(article, chain)
     dallePrompt = ""
     print(story)
 
-    if "DALLE:" in story:
-        summary = story.split("DALLE:")[1]
-        print("DALLE prompt found:")
-        dallePrompt = article
-        
-    else:
-        print("No DALLE prompt")
-        dallePrompt = summariseText(article)
-
+    generateImage(story, article)
     # Generate image
     fullDallePrompt= "2D cartoon child-friendly image with no text of this story: " + dallePrompt
     print("Generating image...")
     image_url = generateImageWithDALLE(fullDallePrompt)
     return story, image_url
+
+def generateNextPage(userOutput, chain):
+    output = continueStory(chain, userOutput)
+    dallePrompt = generateImage(output)
+    image_url = generateImageWithDALLE(dallePrompt)
+    return output, image_url
+
     
-@app.route('/fetch_story/<id>', methods=['GET'])
-def get_story(id):
-        articleContent = getArticleContent(id)
-        if articleContent:
-            story, image_url = generateStoryAndImage(articleContent)
-            return jsonify({"story": story, "image_url": image_url})
-        else:
-            return jsonify({"error": "Article not found"}), 404
     
 if __name__ == "__main__":
     app.run(debug=True)
+    storyChain = initialiseModel()
+
+
--- App/Backend/storyChain.py
+++ App/Backend/storyChain.py
@@ -1,27 +1,65 @@
-from langchain_openai import OpenAI
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain.memory import ConversationKGMemory
+from langchain import ConversationChain
+from langchain.prompts import PromptTemplate
 import os
 #import .env file
 from dotenv import load_dotenv
 
-def storyChain(article):
+#instantiate model so it can be passed between functions
+
+def initialiseModel():
     load_dotenv()
     #get key from .env
     llm = ChatOpenAI(openai_api_key = os.getenv("OPENAI_API_KEY"), model = "gpt-3.5-turbo-0125")
     output_parser = StrOutputParser() #converts output to string
+    memory = ConversationKGMemory(llm=llm)
+    template_text = """
+    The following is a storytelling session based on an article, tailored for children aged 5-10. 
+    The story evolves with interactive elements and educational questions, designed to engage the young reader and encourage critical thinking. 
+    Each segment of the story concludes with a question that directs the narrative, followed by a DALL-E prompt for visualizing the story's current events.
 
-    prompt = ChatPromptTemplate.from_messages([
-        ("system", "You are a young children's (age 5-10) storyteller that takes an input article and creates an educational story out of it."
-        +"For every prompt you: Introduce a summary of the article and what has happened in the first paragraph to a 6 year old kid." 
-        +"Then generate a creative story of 5-8 paragraphs based on the article which explains the main"
-        +"topic of the article which will be given. Finally output a dall-e prompt for an image of the story starting with DALLE: "),
-        ("user", "{input}")
-    ])
+    Relevant Information:
+    {history}
 
-    #Main Chain
-    chain = prompt | llm | output_parser
-    output = chain.invoke({"input": article})
+    Current Interaction:
+    {input}
+
+    Generated Question:
+    {question}
+
+    DALL-E Prompt:
+    {dalle_prompt}
+    """
+
+    prompt = PromptTemplate(input_variables=["history", "input", "question", "dalle_prompt"], template=template_text)
+    #story chain with llm, prompt and memory with output parser as string
+    storyChain = ConversationChain(llm=llm, prompt=prompt, memory=memory, output_parser=output_parser)
+    #storyChain = ConversationChain(llm=llm, prompt=prompt, memory=memory)
+    return storyChain
+
+def initialiseStory(article, storyChain):
+    memory = storyChain.memory
+    #save input article to memory
+    memory.save_context({"input": "Article Summary"}, {"output": article})
+    output = storyChain.predict(input=article)
+    #save the output to memory
+    memory.save_context({"input": "latest"}, {"output": output})
     return output
 
+def continueStory(conversation_chain, user_input):
+    memory = conversation_chain.memory
+    
+    # Load current story context from memory
+    story_context = memory.load_memory_variables({"input": "latest"})
+    
+    # Assuming 'predict' or a similar function uses the current context and user input
+    output = conversation_chain.predict(input=user_input, context=story_context)
+    
+    # Update memory with new story part, question, and DALL-E prompt
+    memory.save_context({"input": user_input}, {"output": output["story"]})
+    memory.save_context({"input": "Question"}, {"output": output["question"]})
+    memory.save_context({"input": "DALL-E Prompt"}, {"output": output["dalle_prompt"]})
+    
+    return output
