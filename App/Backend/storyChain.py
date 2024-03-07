733108b097f5c50b43c127d86808eaad9f17bc30
Shaanukstar123 <shaanukag0@gmail.com>
2024-03-07 04:41:19 +0000

    Conversational Summary Buffer memory implemented (working)

--- App/Backend/storyChain.py
+++ App/Backend/storyChain.py
@@ -4,6 +4,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain.memory import ConversationKGMemory
 from langchain.chains import ConversationChain
 from langchain.prompts.prompt import PromptTemplate
+from langchain.chains.conversation.memory import ConversationSummaryBufferMemory
 import os
 #import .env file
 from dotenv import load_dotenv
@@ -22,23 +23,22 @@ def initialiseModel():
 
     #llm = ChatOpenAI(openai_api_key = os.getenv("OPENAI_API_KEY"), model = "gpt-3.5-turbo-0125")
     output_parser = StrOutputParser() #converts output to string
-    memory = ConversationKGMemory(llm=llm)
-
+    memory = ConversationSummaryBufferMemory(llm=llm)
     promptText = """
-    The following is a continuous interactive educational children's story-telling session based on the topic of a given news article. The story is tailored for children aged 5-10. The article will be introduced first before starting the story.
-    The story will evolve with user interaction as you ask them questions and change the story according to their answers, designed to engage the young reader and encourage critical thinking. 
-    Only one page of the story will be generated each time and history of the context will be tracked.
-    Each segment (page) of the story concludes with 1 question that directs the narrative, followed by a DALL-E prompt for visualizing the story's current events beginning with "DALL-E Prompt:".
+    The following is a continuous interactive educational children's story-telling session based on the topic of a given news article. Generate a page that will continue the story after each
+    prompt input. User input will determine how the story changes in the current page. Continue the story given the history of chat by the conversational summary buffer memory. Ask an educational question 
+    at the end of the page that relates to the story
+    and can change the direction of the narrative. 
+    The article will be introduced first before starting the story.
+    Each segment (page) of the story concludes with 1 question that starts with "Question: ", which directs the narrative, followed by a DALL-E prompt for visualizing the story's current events beginning with "DALL-E Prompt:".
 
-    "The following continues the story based on previous events:
-    {history}
-    Now, considering what has happened so far, {input}
-    Storyteller: "
+    History of chat: {history}
+    User input to the previous question: {input}
     """
 
     prompt = PromptTemplate(input_variables=["history", "input"], output_variables=["output"], template=promptText)
     #story chain with llm, prompt and memory with output parser as string
-    storyChain = ConversationChain(llm=llm, prompt=prompt, memory=memory, output_parser=output_parser)
+    storyChain = ConversationChain(llm=llm, prompt=prompt, memory=memory, output_parser=output_parser, verbose=True)
     #storyChain = ConversationChain(llm=llm, prompt=prompt, memory=memory)
     return storyChain
 
@@ -53,11 +53,11 @@ def saveToMemory(memory, new_content):
 
 def initialiseStory(article, storyChain):
     memory = storyChain.memory
-    initialContext = "Article Summary: " + article
-    saveToMemory(memory, initialContext)
+    initialContext = "First introduce this news article and then begin the educational story about the subject of the article. Article Summary: " + article
+    #saveToMemory(memory, initialContext)
     output = storyChain.predict(input=initialContext)
     #save the output to memory
-    saveToMemory(memory, output)
+    #saveToMemory(memory, output)
     print("Memory: ",memory)
     #memory.save_context({"input": "latest"}, {"output": output})
     return output
@@ -65,16 +65,19 @@ def initialiseStory(article, storyChain):
 def continueStory(storyChain, userInput):
     memory = storyChain.memory
     # Load current story context from memory
-    storyContext = memory.load_memory_variables({"input": "history"})
-    print("Cumulative story context: ",storyContext)
-    cumulativeInput = ""
-    if storyContext.get("history"):
-        cumulativeInput = f"{storyContext['history']} {userInput}"
-    else:
-        cumulativeInput = userInput
+    #storyContext = memory.load_memory_variables({})
+    # print("Cumulative story context: ",storyContext)
+    # cumulativeInput = ""
+    # if storyContext.get("history"):
+    #     cumulativeInput = f"{storyContext['history']} {userInput}"
+    # else:
+    #     cumulativeInput = userInput
     # Assuming 'predict' or a similar function uses the current context and user input
-    output = storyChain.predict(input=cumulativeInput)
+    # output = storyChain.predict(input=cumulativeInput)
+    prompt = '''This is the user output to the question asked in the previous page of the story (stored in conversatinal buffer memory). Continue the story based on the user's answer with a new page and question at the
+    end, or conclude the story if necessary by ending with "THE END". User's answer to previous question:''' + userInput
+    output = storyChain.predict(input=prompt)
     print("Output: ",output)
-    saveToMemory(memory, output)
-    print("Memory: ",memory)
+    #saveToMemory(memory, output)
+    print("MemoryContinued: ",memory)
     return output
